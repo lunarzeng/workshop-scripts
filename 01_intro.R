485ec1356c83504fd7df31eab5f07e9554b9a6cf
lunarzeng <lunarzeng@126.com>
2019-01-15 13:28:20 -0600

    1st R_intro

---
+++ 01_intro.R
@@ -0,0 +1,15 @@
+#import shapefile into R#
+library(sf) #Ctrl+Enter means run#
+
+ward86 <- st_read("data/ward1986/ward1986.shp")  #tab is a shortcut#
+
+st_transform(ward86, "+proj=utm +zone=18 +datum=WGS84 +units=m +no_defs +ellps=WGS84 +towgs84=0,0,0")
+ward86_utm <- st_transform(ward86,32616) #using epsg#
+st_crs(ward86)
+st_crs(ward86_utm)
+ward86_eastplane <- st_transform(ward86,3435) #using epsg#
+st_crs(ward86_eastplane)
+
+plot(ward86)
+plot(ward86_utm)
+plot(ward86_eastplane)
